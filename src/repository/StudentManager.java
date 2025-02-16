fe6bdf1e99ce1e119ae72f22e1afbdfad5e6a1d9
dlwjdtjq001 <dlwjdtjq001@gmail.com>
2025-02-16 16:46:33 +0900

    2025.02.16 Feat: javadoc commenting at repository
    package repository

--- src/repository/StudentManager.java
+++ src/repository/StudentManager.java
@@ -1,37 +1,41 @@
 package repository;
 
 import dto.StudentDto;
-import lombok.Getter;
-import lombok.Setter;
-
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * 캐시 데이터 클래스 (싱글톤패턴 적용)
+ */
 public class StudentManager {
 
 
     private Map<String, StudentDto> studentTable = new HashMap<>();
 
     private static StudentManager studentManagerSingleton = new StudentManager();
-    static StudentDBIO studentDBIO;
 
-    public static void setStudentDBIO(StudentDBIO studentDBIO) {
-        StudentManager.studentDBIO = studentDBIO;
-    }
-
+    /**
+     * 싱글톤으로 캐시데이터를 생성하는 메소드
+     * @return 싱글톤 매니저 객체 반환
+     */
     public static StudentManager getInstance(){ // 싱글톤 패턴 getInstance
         return studentManagerSingleton;
     }
 
+    /**
+     * 캐시 테이블 얻어오는 메소드
+     * @return 학생 정보를 담은 Map
+     */
     public Map<String, StudentDto> getStudentTable() {
         return studentTable;
     }
 
 
-
-    //
+    /**
+     * 캐시 테이즐을 수정하는 메소드
+     * @param studentTable 학생 정보를 담은 map
+     */
     public void setStudentTable(Map<String, StudentDto> studentTable) {
         this.studentTable = studentTable;
-        }
     }
-
+}
