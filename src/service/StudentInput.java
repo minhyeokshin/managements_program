949be9cf8319bce601d81f20293efc5d70978f47
unknown <star919m@g.yju.ac.kr>
2025-02-16 16:05:25 +0900

    0216_edit_input

--- src/service/StudentInput.java
+++ src/service/StudentInput.java
@@ -3,10 +3,5 @@ package service;
 import dto.StudentDto;
 
 public interface StudentInput {
-    String initStudentNumberCounter();
-    int calcTotal(StudentDto studentDto);
-    double calcAverage(StudentDto studentDto);
-    String calcGrade(double average);
     void putStudentTable(StudentDto studentDto);
-    StudentDto createPerfectDto(StudentDto studentDto, String studentNumber);
 }
