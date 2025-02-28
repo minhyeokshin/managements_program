68d9929118ee25cda19a84f584b1841e7e2fed04
Cmk1031 <maple1031@naver.com>
2025-02-28 22:05:13 +0900

    fix: repo 매개변수 DTO -> VO 변경

--- src/employee/repository/EmployeeCreateRepoImp.java
+++ src/employee/repository/EmployeeCreateRepoImp.java
@@ -1,12 +1,13 @@
 package employee.repository;
 
 
-import employee.dto.EmployeeDto;
+
+import employee.vo.EmployeeVo;
 
 public class EmployeeCreateRepoImp implements EmployeeCreateRepo{
 
     @Override
-    public Boolean create(EmployeeDto employeeDto) {
+    public Boolean create(EmployeeVo employeeVo) {
         return null;
     }
 }
