b8807a18b1aad1ba3ccd5984f0949dc31e1461bc
Cmk1031 <maple1031@naver.com>
2025-03-01 17:52:09 +0900

    feat: Employee 정보를 DB에 저장하는 기능 구현

--- src/employee/repository/EmployeeCreateRepo.java
+++ src/employee/repository/EmployeeCreateRepo.java
@@ -2,6 +2,8 @@ package employee.repository;
 
 import employee.vo.EmployeeVo;
 
+import java.sql.SQLException;
+
 public interface EmployeeCreateRepo {
     Boolean create (EmployeeVo employeeVo);
 }
