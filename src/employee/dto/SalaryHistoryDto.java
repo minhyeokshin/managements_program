79f33a13a04b7328710d5c209e5f4f17eb42ab80
Cmk1031 <maple1031@naver.com>
2025-03-03 13:33:55 +0900

    feat: SalaryHistoryDto 추가

--- src/employee/dto/SalaryHistoryDto.java
+++ src/employee/dto/SalaryHistoryDto.java
@@ -1,2 +1,13 @@
-package employee.dto;public class SalaryHistoryDTO {
+package employee.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class SalaryHistoryDto {
+    private Integer eno;
+    private String name;
+    private Integer oldSalary;
+    private Integer newSalary;
 }
