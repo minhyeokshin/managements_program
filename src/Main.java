1431f216971be5049343a608eb4f57a0063fe076
minhyeokshin <sinminhyeok@gmail.com>
2025-03-07 12:20:45 +0900

    250307 feat/controller merge

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
+import employee.controller.MainController;
 import student.controller.StudentOutput;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
         Scanner in = new Scanner(System.in);
 
         DIConfig diConfig = new DIConfig();
